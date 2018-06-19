60a886bbd421739bd3ef2b13cc0f86d3a2495044
ayoub <skobraf@gmail.com>
2018-06-19 15:59:13 +0100

    Initiate file for challenges

---
+++ Front-End-Libraries/React/Create a complex JSX element.js
@@ -0,0 +1,9 @@
+const JSX = <div>
+                <h1>Hello JSX</h1>
+                <p>What are the fron end tools ?</p>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                </ul>
+            </div>
